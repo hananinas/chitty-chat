e7585869e5fe5855797eac617b46511c9d5d3093
Abdul Hanan <hanani.nas@gmail.com>
2023-10-25 17:55:40 +0200

    Added code t create host server to capture clients

--- chat-server/main.go
+++ chat-server/main.go
@@ -1,9 +1,40 @@
 package main
 
+import (
+	"log"
+	"net"
+
+	"github.com/hananinas/chitty-chat/internal/server"
+)
+
 const (
 	port = ":4321"
 	name = "Chit-chat"
 )
 
 func main() {
+	log.Printf("starting chat-server on port %s", port)
+
+	grpcServer, err := server.NewGrpcServer(name)
+	if err != nil {
+		log.Fatalf("could not create server: %v", err)
+	}
+
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("could not listen: %v", err)
+	}
+
+	defer func() {
+		if err := lis.Close(); err != nil {
+			log.Fatalf("could not close listener: %v", err)
+		}
+	}()
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("could not serve: %v", err)
+	}
+
+	log.Printf("Server started %s", name)
+
 }
--- internal/server/main.go
+++ internal/server/main.go
@@ -1,10 +1,61 @@
 package server
 
 import (
+	"log"
+	"sync"
+
+	"github.com/hananinas/chitty-chat/api"
 	pb "github.com/hananinas/chitty-chat/api"
+	"github.com/hananinas/chitty-chat/internal/chat"
+	"google.golang.org/grpc"
 )
 
-// server is used to implement helloworld.GreeterServer.
+type BroadcastSubscription struct {
+	stream   api.ChatService_BroadcastServer
+	finished chan<- bool
+}
+
+// server is used to implement an unimplemented server.
 type server struct {
 	pb.UnimplementedChatServiceServer
+	*Config
+}
+
+// cofniguration for the server
+type Config struct {
+	// keeps a map of all the clients that are connected to the server
+	clients map[string]BroadcastSubscription
+	// mutex to lock the clients map
+	clientsMu sync.Mutex
+	// a clock to keep track of the lamport timestamp of the server
+
+	lamport chat.LamportClock
+	// the name of the server
+	Name string
+}
+
+// NewServer creates a new server
+func NewServer(name string) (*server, error) {
+	chatServer := server{
+		Config: &Config{
+			clients: make(map[string]BroadcastSubscription),
+			Name:    name,
+			lamport: chat.LamportClock{Node: name},
+		},
+	}
+	return &chatServer, nil
+}
+
+// NewGrpcServer creates a new gRPC server and registers the ChatServiceServer
+func NewGrpcServer(name string) (*grpc.Server, error) {
+	grpcServer := grpc.NewServer()
+	s, err := NewServer(name)
+
+	if err != nil {
+		return nil, err
+	}
+
+	api.RegisterChatServiceServer(grpcServer, *s)
+	log.Panicf("Starting server %s", name)
+	return grpcServer, nil
 }
